d80eb59ef47cf308977faa70eb9de091f51991a1
Andre Herman <amelco.herman@gmail.com>
2020-09-01 22:38:49 -0300

    adiciona paredes aleatorias e cantos

--- src/main.rs
+++ src/main.rs
@@ -16,23 +16,13 @@ struct Renderable {
     bg: RGB
 }
 
-#[derive(Component)]
-struct LeftMover {}
-
-#[derive(Component)]
-struct DownMover {}
-
 struct State {
     ecs: World
 }
 
 impl State {
     fn run_systems(&mut self) {
-        let mut lw = LeftWalker{};
-        let mut dw = DownWalker{};
-        lw.run_now(&self.ecs);
-        dw.run_now(&self.ecs);
-        self.ecs.maintain();
+       self.ecs.maintain();
     }
 }
 
@@ -45,36 +35,14 @@ impl GameState for State {
         
         let positions = self.ecs.read_storage::<Position>();
         let renderables = self.ecs.read_storage::<Renderable>();
+        let map = self.ecs.fetch::<Vec<TileType>>();
+
+        draw_map(&map, ctx);
 
         for (pos, render) in (&positions, &renderables).join() {
             ctx.set(pos.x, pos.y, render.fg, render.bg, render.glyph);
         }
 
-    }
-}
-
-struct LeftWalker {}
-impl<'a> System<'a> for LeftWalker {
-    type SystemData = (ReadStorage<'a, LeftMover>,
-                        WriteStorage<'a, Position>);
-
-    fn run(&mut self, (lefty, mut pos): Self::SystemData) {
-        for (_lefty, pos) in (&lefty, &mut pos).join() {
-            pos.x -= 1;
-            if pos.x < 0 { pos.x = 79; }
-        }
-    }
-}
-
-struct DownWalker {}
-impl<'a> System<'a> for DownWalker {
-    type SystemData = (ReadStorage<'a, DownMover>, WriteStorage<'a, Position>);
-
-    fn run(&mut self, (downy, mut pos): Self::SystemData) {
-        for (_downy, pos) in (&downy, &mut pos).join() {
-            pos.y += 1;
-            if pos.y > 49 { pos.y = 0; }
-        }
     }
 }
 
@@ -84,10 +52,14 @@ struct Player {}
 fn try_move_player(delta_x: i32, delta_y: i32, ecs: &mut World) {
     let mut positions = ecs.write_storage::<Position>();
     let mut players = ecs.write_storage::<Player>();
+    let map = ecs.fetch::<Vec<TileType>>();
     
     for (_player, pos) in (&mut players, &mut positions).join() {
-        pos.x = min(79, max(0, pos.x + delta_x));
-        pos.y = min(49, max(0, pos.y + delta_y));
+        let destination_idx = xy_idx(pos.x + delta_x, pos.y + delta_y);
+        if map[destination_idx] != TileType::Wall {
+            pos.x = min(79, max(0, pos.x + delta_x));
+            pos.y = min(49, max(0, pos.y + delta_y));
+        }
     }
 }
 
@@ -104,6 +76,66 @@ fn player_input(gs: &mut State, ctx: &mut Rltk) {
     }
 }
 
+#[derive(PartialEq, Copy, Clone)]
+enum TileType {
+    Wall,
+    Floor
+}
+
+pub fn xy_idx(x: i32, y: i32) -> usize {
+    (y as usize * 80) + x as usize
+}
+
+fn new_map() -> Vec<TileType> {
+    let mut map = vec![TileType::Floor; 80*50];
+
+    // Constroi o muro dos cantos
+    for x in 0..80 {
+        map[xy_idx(x, 0)] = TileType::Wall;
+        map[xy_idx(x, 49)] = TileType::Wall;
+    }
+    for y in 0..50 {
+        map[xy_idx(0, y)] = TileType::Wall;
+        map[xy_idx(79, y)] = TileType::Wall;
+    }
+
+    // Gera um monte de paredes aleatórias
+    let mut rng = rltk::RandomNumberGenerator::new();
+
+    for _ in 0..400 {
+        let x = rng.roll_dice(1, 79);
+        let y = rng.roll_dice(1, 49);
+        let idx = xy_idx(x, y);
+        if idx != xy_idx(40,25) {
+            map[idx] = TileType::Wall;
+        }
+    }
+
+    return map;
+}
+
+fn draw_map(map: &[TileType], ctx: &mut Rltk) {
+    let mut y = 0;
+    let mut x = 0;
+    for tile in map.iter() {
+        // Renderiza o mapa de acordo com o tipo do tile
+        match tile {
+            TileType::Floor => {
+                ctx.set(x, y, RGB::from_f32(0.5, 0.5, 0.5), RGB::from_f32(0., 0., 0.), rltk::to_cp437('.'));
+            },
+            TileType::Wall => {
+                ctx.set(x, y, RGB::from_f32(0., 1., 0.), RGB::from_f32(0., 0., 0.), rltk::to_cp437('#'));
+            },
+        }
+        // move as coordenadas
+        x += 1;
+        if x > 79 {
+            x = 0;
+            y += 1;
+        }
+    }
+}
+
 fn main() {
     use rltk::RltkBuilder;
     let context = RltkBuilder::simple80x50()
@@ -115,9 +147,8 @@ fn main() {
 
     gs.ecs.register::<Position>();
     gs.ecs.register::<Renderable>();
-    gs.ecs.register::<LeftMover>();
-    gs.ecs.register::<DownMover>();
     gs.ecs.register::<Player>();
+    gs.ecs.insert(new_map());
 
 
     gs.ecs
@@ -130,33 +161,6 @@ fn main() {
         })
         .with(Player{})
     .build();
-
-    for i in 0..10 {
-        if i%2 == 0 {
-            gs.ecs
-                .create_entity()
-                .with(Position { x: i * 7, y: 20 })
-                .with(Renderable {
-                    glyph: rltk::to_cp437('☺'),
-                    fg: RGB::named(rltk::RED),
-                    bg: RGB::named(rltk::BLACK),
-                })
-                .with(LeftMover{})
-                .with(DownMover{})
-            .build();
-        } else {
-            gs.ecs
-                .create_entity()
-                .with(Position { x: i * 7, y: 20 })
-                .with(Renderable {
-                    glyph: rltk::to_cp437('☺'),
-                    fg: RGB::named(rltk::RED),
-                    bg: RGB::named(rltk::BLACK),
-                })
-                .with(LeftMover{})
-            .build();
-        }
-    }
 
     rltk::main_loop(context, gs);
 }
